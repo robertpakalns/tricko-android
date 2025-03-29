5878fd5b21e1c8d61e8333d8c94efe1fc74c8845
rob <roby.pakalns@gmail.com>
2025-03-29 12:26:55 +0200

    init

---
+++ app/src/main/java/pro/tricko/android/MainActivity.kt
@@ -0,0 +1,18 @@
+package pro.tricko.android
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://tricko.pro")
+    }
+}
